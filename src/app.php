d4784168030a3b04f8f8cece35f63422b50cb4ec
libingxin <396249450@qq.com>
2016-10-21 11:47:34 +0800

    第一次更新

---
+++ src/app.php
@@ -0,0 +1,80 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../src/cloud.php';
+
+/*
+ * A simple Slim based sample application
+ *
+ * See Slim documentation:
+ * http://www.slimframework.com/docs/
+ */
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+use \Slim\Views\PhpRenderer;
+use \LeanCloud\Client;
+use \LeanCloud\Storage\CookieStorage;
+use \LeanCloud\Engine\SlimEngine;
+use \LeanCloud\Query;
+use \LeanCloud\Object;
+
+$app = new \Slim\App();
+// 禁用 Slim 默认的 handler，使得错误栈被日志捕捉
+unset($app->getContainer()['errorHandler']);
+
+Client::initialize(
+    getenv("LC_APP_ID"),
+    getenv("LC_APP_KEY"),
+    getenv("LC_APP_MASTER_KEY")
+);
+// 将 sessionToken 持久化到 cookie 中，以支持多实例共享会话
+Client::setStorage(new CookieStorage());
+
+SlimEngine::enableHttpsRedirect();
+$app->add(new SlimEngine());
+
+// 使用 Slim/PHP-View 作为模版引擎
+$container = $app->getContainer();
+$container["view"] = function($container) {
+    return new \Slim\Views\PhpRenderer(__DIR__ . "/views/");
+};
+
+$app->get('/', function (Request $request, Response $response) {
+    return $this->view->render($response, "index.phtml", array(
+        "currentTime" => new \DateTime(),
+    ));
+});
+
+// 显示 todo 列表
+$app->get('/todos', function(Request $request, Response $response) {
+    $query = new Query("Todo");
+    $query->descend("createdAt");
+    try {
+        $todos = $query->find();
+    } catch (\Exception $ex) {
+        error_log("Query todo failed!");
+        $todos = array();
+    }
+    return $this->view->render($response, "todos.phtml", array(
+        "title" => "TODO 列表",
+        "todos" => $todos,
+    ));
+});
+
+$app->post("/todos", function(Request $request, Response $response) {
+    $data = $request->getParsedBody();
+    $todo = new Object("Todo");
+    $todo->set("content", $data["content"]);
+    $todo->save();
+    return $response->withStatus(302)->withHeader("Location", "/todos");
+});
+
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+
+$app->run();
+
