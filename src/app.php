caa20bfaee8907b6158a76a09d0dec9a53a7c3ca
libingxin <396249450@qq.com>
2016-10-25 14:18:13 +0800

    0100

--- src/app.php
+++ src/app.php
@@ -177,7 +177,7 @@ $app->post('/register', function(Request $request, Response $response) {
     $res = $response->withHeader("Access-Control-Allow-Origin","*");
     $res = $res->withHeader("Content-Type","application/json;charset=utf-8");
 	$data = $request->getParsedBody();
-	User::become($data["sessT"]);
+	User::become('woxb1ovxamub0cqua2jvv61dw');
 	$currentUser = User::getCurrentUser();
 	$objid = $currentUser -> getObjectId();
 	$currentUser->set("sex","男");
