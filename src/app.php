07ab7bc36b7984c41ea0d93221dd13cae9042d1a
521libingxin <396249450@qq.com>
2016-12-08 10:39:34 +0800

    Update app.php

--- src/app.php
+++ src/app.php
@@ -46,7 +46,8 @@ $app->get('/', function (Request $request, Response $response) {
 $app->get('/index', function (Request $request, Response $response) {
     return $this->view->render($response, "index.phtml", array(
         "currentTime" => new \DateTime(),
-        "ip" =>$request.headers['x-real-ip']
+        "ip" =>$request -> headers['x-real-ip'],
+        "ip2" =>$request -> headers['x-forwarded-for']
     ));
 });
 /*$app->get('/index', function (Request $request, Response $response) {
