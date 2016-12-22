6e133f080a1f66a7e7a7ddae100917e5f5e6db2f
libingxin <396249450@qq.com>
2016-12-22 13:55:07 +0800

    end

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,4 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowser }    from '@angular/platform-browser';
+import { AppModuleNgFactory } from '../aot/app/app.module.ngfactory';
+platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
 
-import { AppModule } from './app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
